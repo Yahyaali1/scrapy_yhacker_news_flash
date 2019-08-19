abadeff67714cbe8876ddc94edb90f527fe6fa57
Yahya Ali <yahya.ali@tintash.com>
2019-08-19 12:58:28 +0500

    Revert "#Inital Commit"
    This reverts commit 8745be8c61bc413a339643ec336e333698f76546.

--- yCombinator/datakeys.py
+++
@@ -1,8 +0,0 @@
-class DataKeys:
-    JOB_TITLE = "job_title"
-    JOB_MSG = "job_msg"
-    JOB_ID = "job_id"
-    JOB_URL = "job_url"
-    POSITION = "position"
-    COMPANY_NAME = "company_name"
-    JOB_LOCATION = "job_location"
