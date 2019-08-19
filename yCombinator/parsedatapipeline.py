6aa0046f9c195ba1b3c469768e68944ffe3244ab
Yahya Ali <yahya.ali@tintash.com>
2019-08-19 12:59:08 +0500

    Revert "Revert "#Inital Commit""
    This reverts commit abadeff67714cbe8876ddc94edb90f527fe6fa57.

---
+++ yCombinator/parsedatapipeline.py
@@ -0,0 +1,47 @@
+import re
+
+from .datakeys import DataKeys
+
+
+class YcombinatorParseDataPipeline(object):
+    def close_spider(self, spider):
+        for job in spider.items:
+            self.parse_data(job)
+
+    def parse_data(self, item):
+        regex_website_split = r"(\.)"
+        regex_sentence_split = r"((((i|I)s |)((h|H)(iring|ires)|((L|l)ooking)))|((w|W)ants )|((r|R)aised)|((s|S)eeking))(\s|)((a|an)|for( a|)|-|)"
+        regex_company_title = r"[(]"
+        regex_location = r"(in\b)"
+        # Split the sentence based on given tokens
+        data_sentence_split = re.split(
+            regex_sentence_split, item["job_msg"], 1)
+        print(data_sentence_split)
+        # Is split was successful extract company name and other data
+        if len(data_sentence_split) > 1:
+            # Extract company data after removing brackets.
+            company_name_split = re.split(
+                regex_company_title, data_sentence_split[0], 1)
+            item[DataKeys.COMPANY_NAME] = company_name_split[0]
+            # Extract job title
+            job_title_split = re.split(
+                regex_location,
+                data_sentence_split[len(data_sentence_split)-1], 1)
+            print(job_title_split)
+            item[DataKeys.POSITION] = job_title_split[0]
+
+            if len(job_title_split) > 1:
+                item[DataKeys.JOB_LOCATION] = job_title_split[len(
+                    job_title_split)-1]
+            else:
+                item[DataKeys.JOB_LOCATION] = "Not Given"
+        else:
+            item[DataKeys.JOB_LOCATION] = "Not Given"
+            if item[DataKeys.COMPANY_NAME] is not None:
+                company_name_split = re.split(
+                    regex_website_split, item[DataKeys.COMPANY_NAME], 1)
+                item[DataKeys.COMPANY_NAME] = company_name_split[0]
+            else:
+                item[DataKeys.COMPANY_NAME] = "Not Given"
+            item[DataKeys.JOB_TITLE] = item[DataKeys.JOB_MSG]
+        print(item)
